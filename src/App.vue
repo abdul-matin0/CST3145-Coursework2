08d569fb6104e6e3b13cc8b658d58623bd2eb0b9
Abdul-matin <madeayo04@gmail.com>
2023-03-17 22:57:59 +0000

    add to cart, go to cart

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,124 @@
-<script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <div id="app">
-    <header>
-      <h1>{{ sitename }}</h1>
-      <button class="disabled card-link btn btn-dark btn-sm px-3"><i
-                            class="fas fa-arrow-right mx-2"></i>Go
-                        to cart</button>
-      <button @click="showCheckout">{{ this.cart.length }}
-        Checkout</button>
-    </header>
-    <main>
-      <component :is="currentView"></component>
-    </main>
+  <div class="container py-5 px-md-5" id="app">
+    <p class="text-center">{{ sitename }}</p>
+    <hr />
+    <div class="row mb-3 text-center">
+      <div class="col-12">
+        <button class="card-link btn btn-dark btn-sm px-3 text-center" @click="showCheckout"><i
+            class="fas fa-arrow-right mx-2"></i>Go
+          to cart {{ this.cart.length }}</button>
+      </div>
+    </div>
+
+    <!-- main -->
+    <div class="row pt-2">
+      <main>
+        <component :is="currentView" :lessonList="lessons" :baseURL="baseURL" @add-to-cart="addToCart" :cart="cart">
+        </component>
+      </main>
+    </div>
   </div>
 </template>
 <script>
-import ProductList from "./components/Lesson.vue";
-import Checkout from "./components/Checkout.vue";
+import LessonComponent from "./components/Lesson.vue";
+import CheckoutComponent from "./components/Checkout.vue";
 export default {
   name: "App",
   data() {
     return {
-      sitename: "Vue.js Pet Depot",
+      sitename: "CST3145",
+      currentView: LessonComponent,
+      lessons: [],
       cart: [],
-      currentView: ProductList
+      sortBy: 'subject',  // default sorting value
+      orderBy: 'ascending', // default order value
+      name: '',
+      search: '',
+      phoneNumber: '',
+      baseURL: 'https://cst3145-env.eba-9sjt9wkg.eu-west-2.elasticbeanstalk.com'
     }
   },
-  components: { ProductList, Checkout },
-
+  components: { LessonComponent, CheckoutComponent },
+  // fetching the lessons in json from the get path
+  created: function () {
+    // using promise and fetch to get a list of lessons
+    this.getLessons();
+  },
   methods: {
+    /// returns a new promise that will be resolved or rejected based on the result of the fetch call.
+    getLessons() {
+      let webstore = this
+      fetch(`${this.baseURL}/lessons`).then(
+        function (response) {
+          response.json().then(
+            function (json) {
+              // pushing lessons in json format into the lessons array
+              webstore.lessons = json;
+            }
+          )
+        });
+    },
+
+    // adds a lesson to cart
+    addToCart(_id) {
+      // find selected lesson id
+      var lesson = this.getLessonById(_id);
+      if (lesson.spaces > 0) {
+        // decrease lesson space
+        --lesson.spaces;
+
+        // get existing item from cart
+        var itemInCart = this.getCartItemFromCartByLessonId(_id);
+
+        if (itemInCart != null) {
+          // update existing item in cart with put
+          ++itemInCart.spaces;
+
+        } else {
+          // adding new item to cart
+          itemInCart = { lessonId: _id, spaces: 1, lesson: lesson };
+          this.cart.push(itemInCart);
+        }
+      }
+    },
+    // removes a lesson from cart
+    removeFromCart(lessonId) {
+      // find selected lesson in cart
+      var itemInCart = this.getCartItemFromCartByLessonId(lessonId);
+
+      if (itemInCart.spaces == 1) {
+        // if just one item space is left, remove item completely from cart
+        var index = this.cart.map(x => x.lessonId).indexOf(lessonId);
+        this.cart.splice(index, 1);
+
+        // redirect user back to home if cart is empty
+        if (this.cart.length <= 0) {
+          this.showCheckout();
+        }
+      } else {
+        // reduce number of spaces of item in cart
+        --itemInCart.spaces;
+      }
+
+      // increase lesson space 
+      var lesson = this.getLessonById(lessonId);
+      ++lesson.spaces;
+    },
+    // get lesson by id
+    getLessonById(_id) {
+      var lesson = this.lessons.find(u => u._id == _id);
+      return lesson;
+    },
+    getCartItemFromCartByLessonId(lessonId) {
+      var item = this.cart.find(u => u.lessonId == lessonId);
+      return item;
+    },
     showCheckout() {
-      if (this.currentView === Checkout)
-        this.currentView = ProductList
-      else this.currentView = Checkout;
+      if (this.currentView === CheckoutComponent)
+        this.currentView = LessonComponent
+      else this.currentView = CheckoutComponent;
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,3 +1,42 @@
 <template>
-    <div>Welcome to the Checkout</div>
+    <div>
+        <!-- lessons -->
+        <div class="col-sm-6 col-md-6 col-lg-4 mt-2" v-for="item in cart">
+            <div class="card card-width rounded-0 shadow-sm">
+                <div class="card-body d-flex justify-content-between">
+                    <div>
+                        <span class="card-text fs-14">Subject:
+                            <b class="card-subtitle mb-2 fs-14">{{ item.lesson.topic }}</b></span><br />
+                        <span class="card-text fs-14">Location:
+                            <b class="card-subtitle mb-2 fs-14">{{ item.lesson.location }}</b></span><br />
+                        <span class="card-text fs-14">Price:
+                            <b class="card-subtitle mb-2 fs-14">£{{ item.lesson.price }}</b></span><br />
+                        <span class="card-text fs-14">Spaces:
+                            <b class="card-subtitle mb-2 fs-14">{{ item.spaces }}</b></span><br />
+                        <span class="card-text fs-18"><i v-bind:class="item.lesson.icon"></i></span><br />
+
+                        <button @click="removeFromCart(item.lessonId)"
+                            class="card-link btn btn-dark btn-sm mt-md-5 px-lg-2">
+                            <i class="fas fa-minus mx-1"></i> Remove
+                        </button>
+                    </div>
+                    <img v-bind:src="baseURL + '/' + item.lesson.imageURL" v-bind:alt="item.lesson.altText"
+                        class="img-fluid border rounded" style="height: 202px; width: 120px; object-fit: cover" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "Checkout",
+    props: ["cart", "baseURL"],
+    methods: {
+        removeFromCart: function (_id) {
+            // emits a custom event that triggers the parent component to add a lesson to cart
+            this.$emit("add-to-cart", _id);
+        }
+    }
+}
+</script>
